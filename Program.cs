3ff7f711f44a056bb21186c139253073e8fa8488
sandaruwagachchi <wagachchisandaru@gmail.com>
2024-02-19 16:46:32 +0530

    Calculate sum and average

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x=80;
+            int y=70;
+            int z=56;
+            int sum;
+            double avg;
+
+            sum = x + y + z;
+            avg = sum / 3;
+
+            Console.WriteLine("SUM: " + sum);
+            Console.WriteLine("AVERAGE: " + avg);
+            Console.ReadKey();
+
+            
+
+        }
+    }
+}
